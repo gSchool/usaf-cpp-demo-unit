5f7c07a7e62fcd170eb2b1d8dbf4ab55a5e6caf2
Ashcoca <ashvincoca@gmail.com>
2020-06-30 08:53:29 -0700

    Initial Commit

---
+++ VendingMachineDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VendingMachineDemo
+//
+//  Created by Ash Coca on 6/30/20.
+//  Copyright © 2020 Galvanize. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
